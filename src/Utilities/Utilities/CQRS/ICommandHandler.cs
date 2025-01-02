9a7cc8a6fa51ab9d0cce8b5de7cf4270a150230f
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2025-01-02 12:05:19 +0200

    - Add Utilities Project - Add ICommand , IQuery interface to be used to apply CQRS

---
+++ src/Utilities/Utilities/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Utilities.CQRS
+{
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+
+    //this interface is used when the command does not return a result
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+    }
+}
